60c44a9a888fcbe36680aef0908bd2ec31e12f04
ArielGad <ariel.gad4@gmail.com>
2019-02-27 22:16:00 +0200

    {/* eslint-disable-next-line */}

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,6 +5,7 @@ const Navbar = () =>{
     return(
         <nav className="nav-wrapper blue darken-3">
             <div className="container">
+                {/* eslint-disable-next-line */}
                 <a className="brand-logo">Let's Play</a>
                 <ul className="right">
                     <li><Link to="/">Home</Link></li>
