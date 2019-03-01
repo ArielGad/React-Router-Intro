16104c02609ee7eaedceccf7b3d73277730bf1b8
ArielGad <ariel.gad4@gmail.com>
2019-03-01 23:45:07 +0200

    understand how to use route params

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar'
-import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
+import Post from './components/Post'
 
 
 
@@ -15,8 +16,10 @@ class App extends Component {
           <div className="App">
             <Navbar />
               <Route exact path="/" component={Home}/>
-              <Route exact path="/about" component={About}/>
-              <Route exact path="/contact" component={Contact}/>
+              <Route path="/about" component={About}/>
+              <Route path="/contact" component={Contact}/>
+              {/*post_id is my made up name, it could be anything*/}
+              <Route path="/:post_id" component={Post}/>
           </div>
         </BrowserRouter>
     );
